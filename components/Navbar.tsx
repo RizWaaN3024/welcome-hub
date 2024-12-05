2a815c87ddb4e22f162ec47d5d80fdaa03ea8b15
RizWaaN3024 <rizwaanans786@gmail.com>
2024-12-05 19:54:27 +0530

    Code Fixes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 const Navbar = () => {
     return (
         <div className="py-10 bg-slate-300">
-            <div className="lg:container mx-auto">
+            <div className="lg:container mx-auto px-4">
                 <h1 className="text-center text-5xl text-gray-600 font-bold">WelcomeHub</h1>
             </div>
         </div>
