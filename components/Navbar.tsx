9320bb4110eb72e3ba59a9b5b6850442067cdcf0
RizWaaN3024 <rizwaanans786@gmail.com>
2024-12-07 20:47:01 +0530

    Test

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@ const Navbar = () => {
     return (
         <div className="py-10 bg-slate-300">
             <div className="lg:container mx-auto px-4">
+                <h2>Hello World</h2>
                 <h1 className="text-center text-5xl text-gray-600 font-bold">WelcomeHub</h1>
             </div>
         </div>
